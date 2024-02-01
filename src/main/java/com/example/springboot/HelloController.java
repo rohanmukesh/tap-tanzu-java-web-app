d2d7e9875331eb6a914f18c4baf694dc35ce8611
rohan mukesh <romukesh@vmware.com>
2024-02-01 10:49:07 -0600

    Test Changes

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu. What!! are you doing a refresh ...";
+		return "Greetings from Spring Boot + Tanzu. are you doing a refresh ...";
 	}
 
 }
